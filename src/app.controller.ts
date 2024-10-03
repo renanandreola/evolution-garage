b5ab4d037d33ed07cafa306baf6c53209c0afcc9
Renan <renanandreolla@outlook.com>
2024-10-03 01:24:18 -0300

    feat: creating prisma conn and DB

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// recebe req http e repassa
+
+@Controller('evogarage')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
